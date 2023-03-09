9cf6537e1bceeb40b7274d79945a5935b2f6406f
KirylK <milk012kir@gmail.com>
2023-03-09 18:27:12 +0300

    Add det method

--- src/matrix/Matrices.java
+++ src/matrix/Matrices.java
@@ -144,4 +144,45 @@ public final class Matrices {
 		return result;
 	}
 	
+	public static double det(Matrix matrix) throws MatrixException {
+		if(matrix == null) {
+			throw new MatrixException(MatrixException.NULL_INSTANCE);
+		}
+		
+		int hs = matrix.getHorizontalSize();
+		if(matrix.getVerticalSize() != hs) {
+			throw new MatrixException(MatrixException.NOT_SQUARE);
+		}
+		double det = 0.0;
+		for(int j = 0; j < hs; j++) {
+			det += matrix.getValue(0, j) * A(matrix, 0, j);
+		}
+		return det;
+	}
+	
+	private static double A(Matrix matrix, int i, int j) throws MatrixException {
+		return Math.pow(-1.0, i + j + 2.0) * M(matrix, i, j);
+	}
+	
+	private static double M(Matrix matrix, int i, int j) throws MatrixException {
+		if(matrix.getVerticalSize() == 1) {
+			return 1.0;
+		}
+		int vs = matrix.getVerticalSize() - 1;
+		int hs = matrix.getHorizontalSize() - 1;
+		Matrix result = new Matrix(vs, hs);
+		for(int ri = 0, k = 0; ri < vs; ri++, k++) {
+			if(k == i) {
+				k++;
+			}
+			for(int rj = 0, l = 0; rj < hs; rj++, l++) {
+				if(l == j) {
+					l++;
+				}
+				result.setValue(matrix.getValue(k, l), ri, rj);
+			}
+		}
+		return det(result);
+	}
+	
 }
