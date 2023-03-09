208920a16440f821a5f0a570bb29c55a9f88317d
KirylK <milk012kir@gmail.com>
2023-03-09 16:45:01 +0300

    Add subtract & add methods

--- src/matrix/Matrices.java
+++ src/matrix/Matrices.java
@@ -41,4 +41,42 @@ public final class Matrices {
 		}
 	}
 	
+	public static Matrix add(Matrix matrix1, Matrix matrix2) throws MatrixException {
+		if(matrix1 == null || matrix2 == null) {
+			throw new MatrixException(MatrixException.NULL_INSTANCE);
+		}
+		
+		int vs = matrix1.getVerticalSize();
+		int hs = matrix1.getHorizontalSize();
+		if(vs != matrix2.getVerticalSize() || hs != matrix2.getHorizontalSize()) {
+			throw new MatrixException(MatrixException.UNEQUAL_SIZE);
+		}
+		Matrix result = new Matrix(vs, hs);
+		for(int i = 0; i < vs; i++) {
+			for(int j = 0; j < hs; j++) {
+				result.setValue(matrix1.getValue(i, j) + matrix2.getValue(i, j), i, j);
+			}
+		}
+		return result;
+	}
+	
+	public static Matrix subtract(Matrix matrix1, Matrix matrix2) throws MatrixException {
+		if(matrix1 == null || matrix2 == null) {
+			throw new MatrixException(MatrixException.NULL_INSTANCE);
+		}
+		
+		int vs = matrix1.getVerticalSize();
+		int hs = matrix1.getHorizontalSize();
+		if(vs != matrix2.getVerticalSize() || hs != matrix2.getHorizontalSize()) {
+			throw new MatrixException(MatrixException.UNEQUAL_SIZE);
+		}
+		Matrix result = new Matrix(vs, hs);
+		for(int i = 0; i < vs; i++) {
+			for(int j = 0; j < hs; j++) {
+				result.setValue(matrix1.getValue(i, j) - matrix2.getValue(i, j), i, j);
+			}
+		}
+		return result;
+	}
+	
 }
